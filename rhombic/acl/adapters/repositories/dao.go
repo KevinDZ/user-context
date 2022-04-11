8e8b7cbdd9bc3b5dbe6b9b00b0efb49c220ac8ba
Kevin <pengjianzhi@realibox.com>
2022-04-11 18:23:13 +0800

    更新系统目录结构

--- rhombic/acl/adapters/repositories/dao.go
+++ rhombic/acl/adapters/repositories/dao.go
@@ -1,21 +1,21 @@
 package repositories
 
 import (
-	dao2 "user-context/rhombic/ohs/local/pl/dao"
-	pl2 "user-context/rhombic/ohs/local/pl/vo"
+	"user-context/rhombic/acl/adapters/pl/dao"
+	"user-context/rhombic/ohs/local/pl/vo"
 )
 
 //LoginRecord 用户登录表 - 写操作
-func LoginRecord(request pl2.LoginRequest) (err error) {
-	var dao dao2.LoginRecordDAO
+func LoginRecord(request vo.LoginRequest) (err error) {
+	var dao dao.LoginRecordDAO
 	_ = dao
 	return
 }
 
 // AccountDAO CQRS原则：账户数据访问对象  - 读操作
-func AccountDAO(request pl2.LoginRequest) (result pl2.LoginRespond) {
+func AccountDAO(request vo.LoginRequest) (result vo.LoginRespond) {
 	// 用户数据表 - 读操作
-	dao := dao2.PostgreSQLDAO{ID: request.ID}
+	dao := dao.PostgreSQLDAO{ID: request.ID}
 	_ = dao
 	return
 }
