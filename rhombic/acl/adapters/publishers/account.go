fc0d198e4d8c2a191804a46e13134908844ac319
彭坚志 <pengjianzhi@realibox.com>
2022-05-11 16:13:05 +0800

    优化注册事件的适配器

--- rhombic/acl/adapters/publishers/account.go
+++ rhombic/acl/adapters/publishers/account.go
@@ -2,7 +2,6 @@ package publishers
 
 import (
 	"github.com/go-redis/redis"
-	"sync"
 	"time"
 	"user-context/rhombic/acl/ports/publishers"
 )
@@ -13,26 +12,26 @@ type AccountEvent struct {
 	*redis.Client //TODO 具体调用方法查看文档
 }
 
-var (
-	once sync.Once
-	pub  publishers.AccountPublisher
-)
+//var (
+//	once sync.Once
+//	pub  publishers.AccountPublisher
+//)
 
 func NewAccountEvent() publishers.AccountPublisher {
-	once.Do(func() {
-		// TODO 事件发布者的实体方法 client.Publish(channel, "")
-		pub = &AccountEvent{redis.NewClient(&redis.Options{})}
-	})
+	//once.Do(func() {
+	// TODO 事件发布者的实体方法 client.Publish(channel, "")
+	pub := &AccountEvent{redis.NewClient(&redis.Options{})}
+	//})
 	return pub
 }
 
 // Registered 注册事件，失败后重试机制的实现逻辑
 func (event AccountEvent) Registered(channel string, msg map[string]string) (err error) {
-	count := 5
+	count := 0
 RETRY:
 	err = event.Publish(channel, msg).Err()
 	if err != nil {
-		for count < 5 {
+		if count > 5 {
 			time.Sleep(time.Duration(2<<count) * time.Second)
 			count++
 			goto RETRY
